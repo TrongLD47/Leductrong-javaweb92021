14fb8e3e3e17f27b2b66ac1984e946814e53686f
TrongLD47 <91546545+TrongLD47@users.noreply.github.com>
2022-01-14 21:58:25 +0700

    Delete src/main directory

--- src/main/java/com/laptrinhjavaweb/constant/SystemConstant.java
+++
@@ -1,8 +0,0 @@
-package com.laptrinhjavaweb.constant;
-
-public class SystemConstant {
-
-    public static final String SUCCESS ="Success";
-    public static final String FAIL ="Fail";
-    public static final String EMPTY_STRING ="";
-}
--- src/main/java/com/laptrinhjavaweb/jdbc/DistrictJdbc.java
+++
@@ -1,9 +0,0 @@
-package com.laptrinhjavaweb.jdbc;
-
-import com.laptrinhjavaweb.entity.DistrictEntity;
-
-import java.util.List;
-
-public interface DistrictJdbc {
-    DistrictEntity findById(Integer id);
-}
